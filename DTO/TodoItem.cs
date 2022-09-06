baf2cb08f3154c6299433987dddc25643dc88b25
Kongsbak99 <victoreskongsbak@gmail.com>
2022-09-06 08:42:16 +0200

    refactoring of porject

--- DTO/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-namespace Api.DTO
-{
-    public class TodoItem
-    {
-        public long Id { get; set; } //The Id property functions as the unique key in a relational database.
-        public string? Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
